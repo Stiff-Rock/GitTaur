a60f38dfbe8057e37e0868b857eaa8c5bf6777e6
Yago <yaguitor@gmail.com>
2025-03-07 19:12:40 +0100

    Icon sizes are not consistent

--- src/components/TitleBar/TitleBar.tsx
+++ src/components/TitleBar/TitleBar.tsx
@@ -1,32 +1,12 @@
-import { listen } from '@tauri-apps/api/event';
 import styles from "./TitleBar.module.css";
-import { GoCodespaces, GoPlus } from "react-icons/go";
-import { invoke } from '@tauri-apps/api/core';
-import type { RepoInfo } from '../../types/repoInfo';
-import { useAppContext } from '../../context/AppContext';
 import WindowTabs from './WindowTabs/WindowTabs';
 import WindowControls from './WindowControls/WindowControls';
 
 const TitleBar: React.FC = () => {
-  const { setRepoInfo } = useAppContext();
-
-  async function openDefault() {
-    const unlisten = await listen<RepoInfo>('repo-info', (event) => {
-      setRepoInfo(event.payload);
-      unlisten();
-    });
-
-    const path = "C:\\Users\\Yago\\Desktop\\PlateaApp";
-    const msg: string = await invoke("open_repository", { path });
-    console.log(msg);
-  }
-
   return (
     <div className={`${styles.titleBar}`}>
       <div className={`${styles.windowTabs}`}>
-        <GoCodespaces className={`${styles.workspaceIcon}`} />
         <WindowTabs />
-        <GoPlus onClick={() => openDefault()} className={`${styles.workspaceIcon}`} />
       </div>
 
       <WindowControls />
--- src/components/TitleBar/WindowTabs/WindowTabs.tsx
+++ src/components/TitleBar/WindowTabs/WindowTabs.tsx
@@ -1,11 +1,31 @@
 import React, { useState } from "react";
 import styles from "./WindowTabs.module.css";
+import { listen } from '@tauri-apps/api/event';
+import { invoke } from '@tauri-apps/api/core';
+import type { RepoInfo } from '../../../types/repoInfo';
+import { GoCodespaces, GoPlus } from "react-icons/go";
+import { useAppContext } from '../../../context/AppContext';
 
 const WindowTabs: React.FC = () => {
+  const { setRepoInfo } = useAppContext();
   const [activeTab, setActiveTab] = useState("Tab1");
+
+  async function openDefault() {
+    const unlisten = await listen<RepoInfo>('repo-info', (event) => {
+      setRepoInfo(event.payload);
+      unlisten();
+    });
+
+    const path = "C:\\Users\\Yago\\Desktop\\PlateaApp";
+    const msg: string = await invoke("open_repository", { path });
+    console.log(msg);
+  }
 
   return (
     <div className={`${styles.tabs}`}>
+
+
+      <GoCodespaces className={`${styles.workspaceIcon}`} />
 
       <div onClick={() => setActiveTab("Tab1")} className={`${styles.tab} ${activeTab === 'Tab1' && styles.active}`}>
         <span>PlateaApp</span>
@@ -14,6 +34,8 @@ const WindowTabs: React.FC = () => {
       <div onClick={() => setActiveTab("Tab2")} className={`${styles.tab} ${activeTab === 'Tab2' && styles.active}`}>
         <span>PythonShit</span>
       </div>
+
+      <GoPlus onClick={() => openDefault()} className={`${styles.workspaceIcon}`} />
 
     </div>
   );
