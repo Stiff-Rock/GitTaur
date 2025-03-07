4f244a67937265715dcbf46bf088dd252152351d
Yago <yaguitor@gmail.com>
2025-03-07 11:29:27 +0100

    Added scrollbar

--- src/components/MainLayout/MainContainer/CommitHistory.tsx
+++ src/components/MainLayout/MainContainer/CommitHistory.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useRepo } from '../../../context/RepoContext';
 import styles from './MainContainer.module.css'
+import { Scrollbars } from 'react-custom-scrollbars-2';
 
 const CommitHistory: React.FC = () => {
   const { repoInfo } = useRepo();
@@ -14,16 +15,23 @@ const CommitHistory: React.FC = () => {
   }, [repoInfo])
 
   return (
-    <div className={`${styles.container}`}>
-      {commitNames.length > 0 ? (
-        commitNames.map((subject, index) => (
-          <span key={index}>{"- " + subject}</span>
-        ))
-      ) : (
-        <p style={{ textAlign: 'center' }}>No commits available</p>
-      )
-      }
-    </div >
+    <Scrollbars
+      autoHide
+      autoHideTimeout={500}
+      autoHideDuration={300}
+      className={styles.scrollContainer}
+    >
+      <div className={`${styles.container}`}>
+        {commitNames.length > 0 ? (
+          commitNames.map((subject, index) => (
+            <span key={index}>{"- " + subject}</span>
+          ))
+        ) : (
+          <p style={{ textAlign: 'center' }}>No commits available</p>
+        )
+        }
+      </div >
+    </Scrollbars>
   );
 };
 
